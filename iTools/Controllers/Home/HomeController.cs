93145c9808a9199cc364683d523c389b194fd32e
嵐嵐 嵐嵐 <stacia10252@gmail.com>
2019-11-09 16:40:15 +0800

    加入BMI功能

--- iTools/Controllers/Home/HomeController.cs
+++ iTools/Controllers/Home/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using iTools.ViewModels.Home;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,33 @@ namespace iTools.Controllers.Home
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult BMI()
+        {
+            BMIViewModel viewModel = new BMIViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult BMI(BMIViewModel viewModel)
+        {
+            decimal iHeight = 0;
+            decimal iWeight = 0;
+            decimal iBMI = 0;
+
+            decimal.TryParse(viewModel.Height, out iHeight);
+            decimal.TryParse(viewModel.Weight, out iWeight);
+
+            iBMI = iWeight / ((iHeight / 100) * (iHeight / 100));
+
+            viewModel.BMI = Math.Round(iBMI, 2, MidpointRounding.AwayFromZero).ToString();
+            
+            return View(viewModel);
         }
     }
 }
---
+++ iTools/ViewModels/Home/BMIViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iTools.ViewModels.Home
+{
+    public class BMIViewModel
+    {
+        public string Height { get; set; }
+        public string Weight { get; set; }
+        public string BMI { get; set; }
+    }
+}
